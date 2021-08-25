86417c9aca89e0e69dcc47f4675c1f8b4a8ddd56
suleman <89507275+sulemanmohd@users.noreply.github.com>
2021-08-25 13:28:18 +0530

    Add files via upload

---
+++ Demo1.js
@@ -0,0 +1,45 @@
+function red()
+{
+	document.getElementById("doc").style.color="red";
+}
+document.getElementById("h1").addEventListener("click",red);
+function green()
+{
+	document.getElementById("doc").style.color="green";
+}
+document.getElementById("h2").addEventListener("click",green);
+function yellow()
+{
+	document.getElementById("doc").style.color="yellow";
+}
+document.getElementById("h3").addEventListener("click",yellow);
+function m20()
+{
+	document.getElementById("doc").style.fontSize="20px";
+}
+document.getElementById("h4").addEventListener("click",m20);
+function m30()
+{
+	document.getElementById("doc").style.fontSize="30px";
+}
+document.getElementById("h5").addEventListener("click",m30);
+function m40()
+{
+	document.getElementById("doc").style.fontSize="40px";
+}
+document.getElementById("h6").addEventListener("click",m40);
+function cursive()
+{
+	document.getElementById("doc").style.fontFamily="cursive";
+}
+document.getElementById("h7").addEventListener("click",cursive);
+function fantasy()
+{
+	document.getElementById("doc").style.fontFamily="fantasy";
+}
+document.getElementById("h8").addEventListener("click",fantasy);
+function serif()
+{
+	document.getElementById("doc").style.fontFamily="serif";
+}
+document.getElementById("h9").addEventListener("click",serif);
